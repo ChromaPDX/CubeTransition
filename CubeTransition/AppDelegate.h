bdf1e8c8d412330846b00216fd8b08d3071db817
structuresound <leif@structuresound.com>
2013-09-18 17:15:34 -0700

    Initial Commit

---
+++ CubeTransition/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CubeTransition
+//
+//  Created by Leif Shackelford on 9/18/13.
+//  Copyright (c) 2013 Leif Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CubeTransition/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CubeTransition
+//
+//  Created by Leif Shackelford on 9/18/13.
+//  Copyright (c) 2013 Leif Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
